4f60bd05da9289c9bcc597681eeb8051e52ef7b0
Piotr Kozerski <piotr.kozerski@pm.me>
2024-07-27 17:35:52 +0200

    Make tasks draggable

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,8 +1,8 @@
-import { useSortable } from "@dnd-kit/sortable";
+import { SortableContext, useSortable } from "@dnd-kit/sortable";
 import TrashIcon from "../icons/TrashIcon";
 import { Column, Id, Task } from "../types"
 import { CSS } from "@dnd-kit/utilities";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import PlusIcon from "../icons/PlusIcon";
 import TaskCard from "./TaskCard";
 
@@ -21,6 +21,10 @@ function ColumnContainer(props: Props) {
     const { column, deleteColumn, updateColumn, createTask, deleteTask, updateTask, tasks } = props;
 
     const [editMode, setEditMode] = useState(false);
+
+    const tasksIds = useMemo(() => {
+        return tasks.map(task => task.id);
+    }, [tasks]);
 
     const { setNodeRef, attributes, listeners, transform, transition, isDragging } = useSortable({
         id: column.id,
@@ -136,9 +140,11 @@ function ColumnContainer(props: Props) {
 
             {/* Tasks container */}
             <div className="flex flex-grow flex-col gap-4 p-2 overflow-x-hidden overflow-y-auto">
-                {tasks.map((task) => (
-                    <TaskCard key={task.id} task={task} deleteTask={deleteTask} updateTask={updateTask} />
-                ))}
+                <SortableContext items={tasksIds}>
+                    {tasks.map((task) => (
+                        <TaskCard key={task.id} task={task} deleteTask={deleteTask} updateTask={updateTask} />
+                    ))}
+                </SortableContext>
             </div>
 
             {/* Add Task button */}
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import TrashIcon from '../icons/TrashIcon';
 import { Id, Task } from '../types';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 
 interface Props {
     task: Task;
@@ -12,13 +14,41 @@ function TaskCard({ task, deleteTask, updateTask }: Props) {
     const [mouseIsOver, setMouseIsOver] = useState(false);
     const [editMode, setEditMode] = useState(false);
 
+    const { setNodeRef, attributes, listeners, transform, transition, isDragging } = useSortable({
+        id: task.id,
+        data: {
+            type: "Task",
+            task
+        },
+        disabled: editMode
+    });
+
+    const style = {
+        transition,
+        transform: CSS.Transform.toString(transform)
+    };
+
     const toggleEditMode = () => {
         setEditMode((prev) => !prev);
         setMouseIsOver(false);
     };
 
+    if (isDragging) {
+        return <div
+            ref={setNodeRef}
+            style={style}
+            className='opacity-50 bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px]
+    items-center flex text-left rounded-xl border-2 border-rose-500 relative'>
+            
+        </div>;
+    }
+
     if (editMode) {
         return <div
+            ref={setNodeRef}
+            style={style}
+            {...attributes}
+            {...listeners}
             className='bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px]
     items-center flex text-left rounded-xl hover-ring-2 hover:ring-inset
     hover:ring-rose-500 relative'
@@ -44,6 +74,10 @@ function TaskCard({ task, deleteTask, updateTask }: Props) {
 
     return (
         <div
+            ref={setNodeRef}
+            style={style}
+            {...attributes}
+            {...listeners}
             onClick={toggleEditMode}
             className='bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px]
         items-center flex text-left rounded-xl hover-ring-2 hover:ring-inset
@@ -52,7 +86,7 @@ function TaskCard({ task, deleteTask, updateTask }: Props) {
             onMouseLeave={() => setMouseIsOver(false)}
         >
             <p
-            className='my-auto h-[90%] w-full overflow-y-auto overflow-x-hidden whitespace-pre-wrap'
+                className='my-auto h-[90%] w-full overflow-y-auto overflow-x-hidden whitespace-pre-wrap'
             >{task.content}</p>
             {mouseIsOver && (
                 <button className="stroke-white absolute right-4 top-1/2
