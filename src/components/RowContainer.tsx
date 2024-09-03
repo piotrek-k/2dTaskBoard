f30c401c9547cb7c912fe7e9e8fb79d0696e07aa
Piotr Kozerski <piotr.kozerski@pm.me>
2024-09-03 21:56:02 +0200

    Added icons for row order management

--- src/components/RowContainer.tsx
+++ src/components/RowContainer.tsx
@@ -4,6 +4,11 @@ import ColumnContainer from './ColumnContainer';
 import { SortableContext } from '@dnd-kit/sortable';
 import RowDetails from './RowDetails';
 import ModalContext, { ModalContextProps } from './modal/ModalContext';
+import MoveUpIcon from '../icons/MoveUpIcon';
+import MoveDownIcon from '../icons/MoveDownIcon';
+import MoveTopIcon from '../icons/MoveTopIcon';
+import MoveBottomIcon from '../icons/MoveBottomIcon ';
+import ArchiveIcon from '../icons/ArchiveIcon';
 
 interface Props {
     row: Row;
@@ -40,18 +45,29 @@ function RowContainer({ row, columns, createTask, getTasks, requestSavingDataToS
             <div className='flex w-full'>
                 <div className='w-[200px] flex-none bg-rowTitleBackgroundColor
                 flex justify-center
-                ' onClick={() => {
-                    handleClickOnRowDetails();
-                }}
-                >
-                    <div className="
+                '>
+                    <div className="flex flex-col">
+                        <div className="
                         bg-mainBackgroundColor
                         w-[150px] 
                         p-2.5
                         m-[12px]
                         h-[100px]
-                        ">
-                        {row.title}
+                        " onClick={() => {
+                                handleClickOnRowDetails();
+                            }}>
+                            {row.title}
+                        </div>
+
+                        <div className='flex flex-grow'></div>
+
+                        <div className='flex flex-row flex-none p-2.5 text-gray-500'>
+                            <MoveUpIcon />
+                            <MoveDownIcon />
+                            <MoveTopIcon />
+                            <MoveBottomIcon />
+                            <ArchiveIcon />
+                        </div>
                     </div>
                 </div>
 
