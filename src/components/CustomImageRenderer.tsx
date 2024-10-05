29b32aa33e18f2a4fa15e9830f878b8e64eac5e2
Piotr Kozerski <piotr.kozerski@pm.me>
2024-10-05 16:30:21 +0200

    Ability to open attachments and add them to task content

--- src/components/CustomImageRenderer.tsx
+++ src/components/CustomImageRenderer.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { IAppStorageAccessor } from '../services/FileSystemStorage';
 import DataStorageContext from './filesystem/DataStorageContext';
 import { Id } from '../types';
@@ -16,29 +16,14 @@ function CustomImageRenderer({ taskId, props }: Props) {
         const fetchCustomSrc = async () => {
             const directory = await dataStorageContext.getDirectoryHandleForTaskAttachments(taskId);
 
-            const src = await getCustomSrc(props.src, directory);
+            const src = await dataStorageContext.mapSrcToFileSystem(props.src, directory);
             setCustomSrc(src);
         };
-
 
         fetchCustomSrc();
     }, [props.src]);
 
-    async function getCustomSrc(originalSrc: string | undefined, directory: FileSystemDirectoryHandle): Promise<string> {
-        if (!originalSrc) return '';
-
-        try {
-            const fileName = originalSrc.split('/').pop();
-            if (!fileName) return originalSrc;
-
-            const fileHandle = await directory.getFileHandle(fileName);
-            const file = await fileHandle.getFile();
-            return URL.createObjectURL(file);
-        } catch (error) {
-            console.error('Error getting custom src:', error);
-            return originalSrc;
-        }
-    }
+    
 
     return (
         <img
@@ -46,7 +31,6 @@ function CustomImageRenderer({ taskId, props }: Props) {
             src={customSrc}
             style={{ maxHeight: '20rem' }}
             loading="lazy"
-            alt={props.alt || 'Custom image'}
         />
     );
 }
