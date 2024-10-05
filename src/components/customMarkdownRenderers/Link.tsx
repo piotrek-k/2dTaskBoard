98742a72ff1494d87a663604c4196ee1c4cd694d
Piotr Kozerski <piotr.kozerski@ceneo.pl>
2024-10-05 18:10:05 +0200

    ci/cd fix

--- src/components/customMarkdownRenderers/Link.tsx
+++ src/components/customMarkdownRenderers/Link.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { IAppStorageAccessor } from '../../services/FileSystemStorage';
 import { Id } from '../../types';
 import DataStorageContext from '../filesystem/DataStorageContext';
