f8310ad8c35402deb5f2bd88af7f78c07037e62d
Piotr Kozerski <piotr.kozerski@ceneo.pl>
2024-10-05 22:55:30 +0200

    Styling improvements

--- src/components/modal/ModalComponent.tsx
+++ src/components/modal/ModalComponent.tsx
@@ -20,16 +20,18 @@ function ModalComponent() {
       overlayClassName="fixed inset-0 bg-black bg-opacity-50"
     >
       <div className="relative bg-gray-800 dark:bg-gray-900 text-white rounded-lg shadow-xl w-full max-w-7xl max-h-[90vh] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-gray-800">
-        <button
-          onClick={handleCloseModal}
-          className="absolute top-2 right-2 text-gray-400 hover:text-gray-200 transition-colors duration-200"
-          aria-label="Close modal"
-        >
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-          </svg>
-        </button>
-        <div className="p-6">
+        <div className="sticky top-0 z-10 bg-gray-950 p-2 flex justify-end">
+          <button
+            onClick={handleCloseModal}
+            className="text-gray-400 hover:text-gray-200 transition-colors duration-200"
+            aria-label="Close modal"
+          >
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+            </svg>
+          </button>
+        </div>
+        <div className="pt-2 pb-6 px-6">
           {modalContent}
         </div>
       </div>
