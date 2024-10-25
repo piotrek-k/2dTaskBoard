adbd099f1c8a41f60b4bec359d4304d1f3563abb
Piotr Kozerski <piotr.kozerski@pm.me>
2024-10-25 23:45:19 +0200

    Working electron app

---
+++ desktop/index.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const localServerApp = express();
+const PORT = 8088;
+const startLocalServer = (done) => {
+  localServerApp.use(express.json({ limit: "100mb" }));
+  localServerApp.use(cors());
+  localServerApp.use(express.static('../web/dist/'));
+  localServerApp.listen(PORT, async () => {
+    console.log("Server Started on PORT ", PORT);
+    done();
+  });
+};
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:" + PORT);
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  startLocalServer(createWindow);
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
