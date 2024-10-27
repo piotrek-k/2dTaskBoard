5340c3786ae492f15eede092349a23a27f6dfc15
Piotr Kozerski <piotr.kozerski@ceneo.pl>
2024-10-27 14:50:35 +0100

    Added option to install as PWA

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '2d task board',
+        short_name: '2d task board',
+        description: '2d task board',
+        theme_color: '#1f2937',
+        icons: [
+          {
+            src: '/logo2.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    })
+  ],
   build: {
     chunkSizeWarningLimit: 1600
   }
