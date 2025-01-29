a327000c9d537af5edaaa63cb26bcc07a982823a
Piotr Kozerski <piotr.kozerski@pm.me>
2025-01-29 21:05:45 +0100

    Electron - fixed problem of possibly taken port

--- desktop/index.js
+++ desktop/index.js
@@ -2,22 +2,62 @@ const { app, BrowserWindow } = require("electron");
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const net = require("net");
+
 const localServerApp = express();
-const PORT = 8088;
-const startLocalServer = (done) => {
-  localServerApp.use(express.json({ limit: "100mb" }));
-  localServerApp.use(cors());
-  localServerApp.use(express.static(__dirname + '/dist'));
-  localServerApp.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+const BASE_PORT = 13000;
+
+let actualPort = BASE_PORT;
+
+const findAvailablePort = (port, callback) => {
+  console.log("Trying another port...", port);
+
+  const server = net.createServer();
+  // server.unref();
+  server.on("error", () => {
+    if(port > BASE_PORT + 10) {
+      console.error("Could not find an available port");
+      process.exit(1);
+    }
+
+    findAvailablePort(port + 1, callback);
   });
-  localServerApp.listen(PORT, async () => {
-    console.log("Server Started on PORT ", PORT);
-    done();
+  server.listen(port, () => {
+    server.close(() => {
+      callback(port);
+    });
   });
 };
 
-function createWindow() {
+const startLocalServer = (port, done, fallback = true) => {
+  localServerApp.use(express.json({ limit: "100mb" }));
+  localServerApp.use(cors());
+  localServerApp.use(express.static(__dirname + "/dist"));
+  localServerApp.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+  });
+
+  const server = localServerApp.listen(port, () => {
+    console.log("Server Started on PORT", port);
+    actualPort = port;
+    done(port);
+  });
+
+  server.on("error", (err) => {
+    if (fallback && err.code === "EADDRINUSE") {
+      console.log(`Port ${port} is in use, searching for a free port...`);
+      findAvailablePort(port + 1, (availablePort) => {
+        startLocalServer(availablePort, done, false);
+      });
+    } else {
+      console.error("Failed to start server:", err);
+    }
+  });
+};
+
+function createWindow(port) {
+  console.log("Using port: ", port);
+
   const mainWindow = new BrowserWindow({
     width: 1000,
     height: 600,
@@ -26,17 +66,17 @@ function createWindow() {
     },
   });
 
-  mainWindow.loadURL("http://localhost:" + PORT);
+  mainWindow.loadURL(`http://localhost:${port}`);
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
-  startLocalServer(createWindow);
+  startLocalServer(BASE_PORT, createWindow);
 
   app.on("activate", function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) createWindow(actualPort);
   });
 });
 
