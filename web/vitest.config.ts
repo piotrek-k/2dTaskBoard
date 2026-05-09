507c372bfa01de4aa0c3fc896cd8666bcfd6d532
Piotr Kozerski <piotr.kozerski@pm.me>
2026-05-09 18:38:37 +0200

    Exclude e2e tests from vitest setup

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     setupFiles: './tests/testSetup.ts',
+    exclude: ['e2e/**', 'node_modules/**'],
   },
 });
